2e2d07738217235d116375c0f6e2ff26c2971921
Gradyn Nagle <gradyndnagle@outlook.com>
2024-03-27 19:44:35 -0700

    added reciept validator middleware

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const app = express();
 
+const app = express();
 const PORT = 3000;
 
-app.post('/reciepts/process', (req, res) => {
-    res.status(200).send('Hello /reciepts/process');
+app.use(express.json());
+
+const receiptValidator = (req, res, next) => {
+    const receipt = req.body;
+    if (!receipt.retailer || !receipt.purchaseDate || !receipt.purchaseTime || !receipt.items || !receipt.total) {
+        return res.status(400).send('Reciept missing required fields.');
+    }
+    next();
+};
+
+app.post('/receipts/process', receiptValidator, (req, res) => {
+    res.status(200).send('Hello /receipts/process');
 });
 
-app.get('/reciepts/:id/points', (req, res) => {
+app.get('/receipts/:id/points', (req, res) => {
     const id = req.params.id;
-    res.status(200).send(`Hello /reciepts/${id}/points`);
+    res.status(200).send(`Hello /receipts/${id}/points`);
 });
 
 app.listen(PORT, () => {
