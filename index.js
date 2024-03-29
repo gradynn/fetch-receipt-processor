5c7a26ef24ab0837004b8b1ee055a69dcce531d1
Gradyn Nagle <gradyndnagle@outlook.com>
2024-03-28 20:51:12 -0700

    cleaned up code, added documenting comments

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import dotenv from 'dotenv';
+import rateLimit from 'express-rate-limit';
 
 import { calculatePoints } from './util.js';
 
+dotenv.config();
+
+// Setup
 const app = express();
 const pointsStore = new Map();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Rate Limiting
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+});
+app.use(limiter);
 app.use(express.json());
 
-const receiptValidator = (req, res, next) => {
+// Validator Middleware
+const receiptSchemaValidator = (req, res, next) => {
     const receipt = req.body;
     if (!receipt.retailer || !receipt.purchaseDate || !receipt.purchaseTime || !receipt.items || !receipt.total) {
         return res.status(400).send('Reciept missing required fields.');
@@ -17,7 +29,12 @@ const receiptValidator = (req, res, next) => {
     next();
 };
 
-app.post('/receipts/process', receiptValidator, (req, res) => {
+/**
+ * POST /receipts/process
+ * 
+ * Process a receipt, calculate points and store points in the pointsStore.
+ */
+app.post('/receipts/process', receiptSchemaValidator, (req, res) => {
     const reciept = req.body;
 
     const id = uuidv4();
@@ -29,6 +46,13 @@ app.post('/receipts/process', receiptValidator, (req, res) => {
     });
 });
 
+
+/**
+ * GET /receipts/:id/points
+ * 
+ * Get the points for a receipt.
+ * @param {string} id - The id of the receipt.
+ */
 app.get('/receipts/:id/points', (req, res) => {
     const id = req.params.id;
 
--- util.js
+++ util.js
@@ -1,3 +1,35 @@
-export const calculatePoints = (reciept) => {
-    return reciept.total * 10;
-}
+/**
+ * Takes a receipt object and returns the total points earned based on various criteria.
+ * @param {Object} receipt The receipt object to calculate points from.
+ * @returns {number} The total points earned from the receipt.
+ */
+export const calculatePoints = (receipt) => {
+    const alphanumericCharCount = 
+        receipt.retailer.match(/[A-Za-z0-9]/g)?.length || 0;
+    const totalAsNumber = Number(receipt.total);
+
+    const fromRetailer = alphanumericCharCount;
+    const fromTotalIsRoundNumber = totalAsNumber % 1 === 0 ? 50 : 0;
+    const fromTotalIsMultiple = totalAsNumber % 0.25 === 0 ? 25 : 0;
+    const fromItems = Math.floor(receipt.items.length / 2) * 5;
+
+    const fromItemsDescription = receipt.items.reduce((acc, item) => {
+        const trimmedLength = item.shortDescription.trim().length;
+        return acc + 
+            (trimmedLength % 3 === 0 ? Math.ceil(Number(item.price) * 0.2) : 0);
+    }, 0);
+
+    const purchaseDate = parseInt(receipt.purchaseDate.split('-')[2]);
+    const fromPurchaseDate = purchaseDate % 2 === 1 ? 6 : 0;
+
+    const purchaseHour = parseInt(receipt.purchaseTime.split(':')[0]);
+    const fromPurchaseTime = purchaseHour >= 14 && purchaseHour < 16 ? 10 : 0;
+
+    return fromRetailer + 
+        fromTotalIsRoundNumber + 
+        fromTotalIsMultiple + 
+        fromItems + 
+        fromItemsDescription + 
+        fromPurchaseDate + 
+        fromPurchaseTime;
+};
